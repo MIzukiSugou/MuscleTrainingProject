e54736ca122479c48a7b3e2a20ab64bc215aa332
github MIzukiSugou <sugou106977@gmail.com>
2021-07-09 23:23:49 +0900

    total計算

--- MuscleTrainingProject/src/main/resources/static/js/trainingRecord.js
+++ MuscleTrainingProject/src/main/resources/static/js/trainingRecord.js
@@ -2,23 +2,39 @@
  * トレーニング記録画面
  */
 
-const menuMapInitial = "トレーニングメニューを選択してください";
-
 window.addEventListener('DOMContentLoaded', function() {
 	
-	// select要素を取得
-	var select_menu = document.querySelector("select[name=menu]");
-
-	// 入力項目要素を取得
-	let menuInputButton = document.getElementById('menuInputButton');
-
-	// 現在の display プロパティの値を保持
-	const displayInputButton = menuInputButton.style.display;
+	//プルダウンが"トレーニングメニューを選択してください"のkey
+	const menuMapInitial = "000";
 	
+	//ボタン制御フラグ：活性
+	const BTNCONTROLACTIVITY = "1";
+	
+	//ボタン制御フラグ：非活性
+	const BTNCONTROLINACTIVE = "0";
+	
+	//totalの自動計算
+	var stName = "st"
+	var total = "total_";
+	
+	//trainingRecordListのサイズ分繰り返し実行
+	for (var index = 0; index < document.getElementById("trainingRecordListSize").value; index++) {
+		for (var stIndex = 1; stIndex <= 6; stIndex++){
+			var idName = stName + stIndex + "_" + index;
+			document.getElementById(idName).onchange = function(){
+  				totalKeisan(stName,total);
+			};
+		}
+	}
+	
+	// select要素を取得
+	var select_menu = document.querySelector("select[name=menuKey]");
 	
 	// ページ読み込み時の処理
 	window.onload = function(){
+		recordBtn();
 		selectChange();
+		totalKeisan(stName,total);
 	}
 	
 	//プルダウン変更時の処理
@@ -28,19 +44,46 @@ window.addEventListener('DOMContentLoaded', function() {
 	
 	//プルダウン状態での入力項目の表示：非表示
 	function selectChange() {
-				if (select_menu.value != menuMapInitial) {
-			//プルダウンが"トレーニングメニューを選択してください"以外の場合、入力フォーム表示
-			menuInputButton.style.display = displayInputButton;
+		
+		if (select_menu.value != menuMapInitial) {
+			//プルダウンが"トレーニングメニューを選択してください"以外の場合、disabled属性を削除
+			document.getElementById("menuInputButton").removeAttribute("disabled");
 			
 			console.log(select_menu.value);
 	
 		} else {
-			//プルダウンが"トレーニングメニューを選択してください"の場合、入力フォーム非表示
-			menuInputButton.style.display = 'none';
-			
-			console.log(select_menu.value);
+			//プルダウンが"トレーニングメニューを選択してください"の場合、disabled属性を設定
+			document.getElementById("menuInputButton").setAttribute("disabled", true);
 			
 		}
 	}
+	
+	//記録ボタンの活性・非活性制御
+	function recordBtn(){
+	
+		if (document.getElementById("recordBtnControl").value === BTNCONTROLACTIVITY){
+			// disabled属性を削除
+			document.getElementById("recordBtn").removeAttribute("disabled");
+			
+		}else if (document.getElementById("recordBtnControl").value === BTNCONTROLINACTIVE) {
+			// disabled属性を設定
+			document.getElementById("recordBtn").setAttribute("disabled", true);
+		}
+	}
+
+	
+	//totalの自動計算
+	function totalKeisan(stName,total) {
+		//trainingRecordListのサイズ分繰り返し実行
+		for (var index = 0; index < document.getElementById("trainingRecordListSize").value; index++) {
+			countTotal = 0;
+			for (var stIndex = 1; stIndex <= 6; stIndex++){
+				var idName = stName + stIndex + "_" + index;
+				countTotal += Number(document.getElementById(idName).value);
+			}
+			document.getElementById(total + index).value = countTotal;	
+		}
+	}
+	
 });
 
